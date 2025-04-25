859e70ff3497c8dd470c9641072ee6ccccc8ad5c
hbehera27 <harishb2727@gmail.com>
2025-04-25 20:34:52 +0530

    add button work

--- src/component/AddTitlePassword.js
+++ src/component/AddTitlePassword.js
@@ -2,18 +2,28 @@ import React from 'react';
 import "./AddTitlePassword.css";
 
 const AddTitlePassword = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const password = event.target.password.value;
+        console.log("Title:", title);
+        console.log("Password:", password);
+    }
+
     return (
-        <div>
-            <div className="field-group">
-                <label htmlFor="title">Title:</label>
-                <input id="title" type="text" placeholder="Enter title" required />
+        <>
+            <div className="field-group" >
+                <form onSubmit={handleSubmit}>
+                   <label htmlFor="title">Title:</label>
+                    <input id="title" type="text" placeholder="Enter title" required />
+                    <br />
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" placeholder="Enter password" required />
+                    <br />
+                    <button type="submit">Add</button> 
+                </form>
             </div>
-            <div className="field-group">
-                <label htmlFor="password">Password:</label>
-                <input id="password" type="password" placeholder="Enter password" required />
-            </div>
-            <button type="submit">Add</button>
-        </div>
+        </>
     );
 };
 
